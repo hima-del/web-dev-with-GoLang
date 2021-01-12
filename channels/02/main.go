133e1a6a4321933e3a3ab127436fbb6c7f858855
hima-del <himajaharidas77@gmail.com>
2021-01-12 22:49:30 +0530

    channels

---
+++ channels/02/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	c := make(chan int)
+	fmt.Println(isClosed(c))
+	close(c) //when the channel is closed you cannnot pou values onto the channel but you can still recieve values from that channel
+	fmt.Println(isClosed(c))
+}
+
+func isClosed(ch <-chan int) bool {
+	select {
+	case <-ch:
+		return true
+	default:
+	}
+	return false
+}
